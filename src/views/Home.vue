48ae2b728af6aa4f5db877d60a5302ffedf2e163
goldt <admin@koalatea-software.com>
2021-04-22 08:17:38 +0100

    made logo, and changed the favicon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo200.gif">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
