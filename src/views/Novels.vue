1461679b8bd42c81e9e2397f5231f06819aa85b4
goldt <admin@koalatea-software.com>
2021-05-03 10:59:17 +0100

    Set up Novels chapter to contain an editable div

--- src/views/Novels.vue
+++ src/views/Novels.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="novels">
-    <h1>Novels and their films</h1>
+    <h1>Novels and their Films</h1>
+    <EditableDiv identity="novels-1"></EditableDiv>
+    <div>
+      Here be a blog extract (general blog)
+    </div>
   </div>
 </template>
+<script>
+import EditableDiv from "@/components/EditableDiv";
+
+export default {
+  components: {EditableDiv}
+}
+</script>
