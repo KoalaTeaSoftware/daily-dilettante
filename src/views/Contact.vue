e471b6e4c06d62dace13881e48c9ed7c5df84108
goldt <admin@koalatea-software.com>
2021-05-04 19:00:13 +0100

    The contact form's validation seems to be substantially in place now.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,102 +1,198 @@
 <template>
   <div>
     <h1>Contact</h1>
-    <form method="post" onsubmit="return checkForm()" action="/chapters/contact/sendmail.php">
+    <b-form
+        @submit="onSubmit"
+        @reset="onReset"
+    >
       <!--suppress HtmlFormInputWithoutLabel -->
       <input hidden id="whadyano" value="<?= $timeNow ?>">
-      <div class="form-group">
-        <label :for="this.nameFieldName">Your Name</label>
-        <input class="form-control"
-               required
-               :id="this.nameFieldName"
-               :name="this.nameFieldName"
-               :maxlength="this.maxNameLength"
-               :pattern="this.nameRegex"
-               type="text"
-               placeholder="Please tell me your name"
-               autofocus>
-      </div>
-      <div class="form-group">
-        <label for="this.email1FieldName">Email address</label>
-        <input class="form-control"
-               required
-               id="this.email1FieldName"
-               name="this.email1FieldName"
-               maxlength="this.maxEmailLength"
-               type="email"
-               placeholder="Please enter your email"
-               onblur="checkEmails()"
-               aria-describedby="emailHelp">
-        <small class="form-text text-muted" id="emailHelp">We'll never share your email with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label for="this.email2FieldName">Email address confirmation</label>
-        <input class="form-control"
-               required
-               id="this.email2FieldName"
-               name="this.email2FieldName"
-               maxlength="this.maxEmailLength"
-               type="email"
-               placeholder="Please re-enter your email"
-               onblur="checkEmails()"
-               aria-describedby="emailConfHelp">
-        <small class="form-text text-muted" id="emailConfHelp">Please type it again, just to check.</small>
-      </div>
+      <b-form-input
+          v-model="formData.name"
+          :state="checkName"
+          pattern="[A-Za-z0-9 .-]+"
+          required
+          id="name"
+          name="name"
+          maxlength="50"
+          type="text"
+          placeholder="Please tell me your name"
+          autofocus></b-form-input>
+      <b-form-invalid-feedback :state="checkName">
+        This must be between 5 and 50 chars long, and be only letters, numbers, similar sensible characters.
+      </b-form-invalid-feedback>
 
-      <div class="form-group">
-        <label for="this.subjectFieldName">Subject</label>
-        <input class="form-control"
-               required
-               id="this.subjectFieldName"
-               name="this.subjectFieldName"
-               maxlength="this.maxSubjectLength"
-               value="this.subject"
-               type="text"
-               placeholder="RE: something ... "
-               aria-describedby="subjectHelp">
-        <small class="form-text text-muted" id="subjectHelp">Please provide a subject.</small>
-      </div>
-      <div class="form-group">
-        <label>Your Message: (chars left: <span id="letterCount">this.msgMaxLen ?></span>)
-          <textarea class="form-control"
-                    required
-                    id="this.messageFieldName"
-                    name="this.messageFieldName"
-                    minlength="this.msgMinLen"
-                    maxlength="this.msgMaxLen"
-                    rows="5" cols="180"
-                    onkeyup="showCount(this.value)"></textarea>
-        </label>
-      </div>
-      <button class="btn btn-lg" type="submit" name="this.submitButtonIdName">Send</button>
-    </form>
+      <b-form-input
+          v-model="formData.address1"
+          :state="checkEmails"
+          required
+          id="address1"
+          name="address1"
+          maxlength="50"
+          type="email"
+          placeholder="Please tell me your email address"
+          autofocus></b-form-input>
+      <b-form-invalid-feedback :state="checkEmails">
+        Please ensure that a valid email address is provided
+      </b-form-invalid-feedback>
+
+      <b-form-input
+          v-model="formData.address2"
+          :state="checkEmails"
+          required
+          id="address2"
+          name="address2"
+          maxlength="50"
+          type="email"
+          placeholder="Please tell confirm your email address"
+          autofocus></b-form-input>
+      <b-form-invalid-feedback :state="checkEmails">
+        Please ensure that both email address fields contain identical data
+      </b-form-invalid-feedback>
+
+      <b-form-input
+          v-model="formData.subject"
+          :state="checkSubject"
+          pattern="[A-Za-z0-9 .-]+"
+          required
+          id="subject"
+          name="subject"
+          maxlength="50"
+          type="text"
+          placeholder="Please provide some subject for this message"
+          autofocus></b-form-input>
+      <b-form-invalid-feedback :state="checkSubject">
+        This must be between 5 and 50 chars long, and be only letters, numbers, and similar ordinary characters.
+      </b-form-invalid-feedback>
+
+      <b-form-textarea
+          v-model="formData.message"
+          :state="checkMessage"
+          @keyup="showCount"
+          pattern="[A-Za-z0-9 .-]+"
+          required
+          id="message"
+          name="message"
+          placeholder="Enter something..."
+          rows="10"></b-form-textarea>
+      <b-form-invalid-feedback :state="checkMessage">
+        This must be between 10 and 5,000 chars long, and be only letters, numbers, and similar ordinary characters.
+      </b-form-invalid-feedback>
+
+      <span id="counter">(Chars left: <span id="letterCount">500</span>)</span>
+      <b-button type="submit" id="submitButton">Submit</b-button>
+      <b-button type="reset" id="resetButton">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
+<style scoped lang="scss">
+@import "src/assets/livery.scss";
+
+form {
+  width: 75%;
+  margin: auto;
+
+  input, textarea {
+    margin: 1em;
+  }
+
+  .erroneousField {
+    border-color: red;
+    color: red;
+  }
+
+  #counter {
+    float: right;
+    font-size: smaller;
+    font-style: italic;
+  }
+
+  #submitButton {
+    background-color: $colour-banner-background;
+    color: $colour-banner-primary-text;
+    margin: 1em;
+    float: left;
+  }
+
+  #resetButton {
+    background-color: transparent;
+    color: $colour-banner-background;
+    margin: 1em;
+    float: left;
+  }
+
+}
+
+</style>
+
 <script>
+/*
+maxNameLength: 40,
+maxEmailLength: 50,
+maxSubjectLength: 50,
+msgMaxLen: 5000,
+msgMinLen: 10,
+ */
 export default {
   name: "Contact",
   data() {
     return {
-      maxNameLength: 40,
-      maxEmailLength: 50,
-      maxSubjectLength: 50,
-      msgMaxLen: 5000,
-      msgMinLen: 10,
-
-      nameFieldName: "name",
-      email1FieldName: "emailAddress",
-      email2FieldName: "emailAddressConf",
-      subjectFieldName: "subject",
-      messageFieldName: "yourMessage",
-      submitButtonIdName: "sendMsg",
-
-      nameRegex: "[A-Za-z0-9 .\-]+"
+      formData: {
+        name: "",
+        address1: "",
+        address2: "",
+        subject: "",
+        message: ""
+      }
+    }
+  },
+  methods: {
+    onSubmit: function (event) {
+      event.preventDefault()
+      if (this.checkEmails && this.checkName)
+        alert(JSON.stringify(this.formData))
+    },
+    onReset: function (event) {
+      console.log("Resetting")
+      event.preventDefault()
+      this.formData.name = this.formData.address1 = this.formData.address2 = this.subject = this.message = ""
+    },
+    setFieldHighlight: function (element) {
+      element.classList.add("erroneousField");
+    },
+    clearFieldHighlight: function (element) {
+      element.classList.remove("erroneousField");
+    },
+    showCount: function () {
+      document.getElementById("letterCount").innerText =
+          (500 - document.getElementById('message').value.length).toString()
+    }
+  },
+  computed: {
+    checkName: function () {
+      return (
+          (this.formData.name.length > 5) &&
+          (this.formData.name.length < 51) && (this.formData.name.match(/^[a-z0-9., -]+$/i) != null)
+      )
+    },
+    checkEmails: function () {
+      // rely on the browser to validate the email formats
+      return (this.formData.address1.length > 4 && this.formData.address1 === this.formData.address2)
+    },
+    checkSubject: function () {
+      return (
+          (this.formData.subject.length > 5) &&
+          (this.formData.subject.length < 51) && (this.formData.subject.match(/^[£a-z0-9., -]+$/i) != null)
+      )
+    },
+    checkMessage: function () {
+      return (
+          (this.formData.message.length > 10) &&
+          (this.formData.message.length < 5001) && (this.formData.message.match(/^[£a-z0-9., -/?/)(]+$/i) != null)
+      )
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
