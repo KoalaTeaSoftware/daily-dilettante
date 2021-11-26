3447c67d3c1b8c4a2dc55b612cf65b1371523b50
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2021-11-26 06:37:02 +0000

    improve layout on the novels page

--- src/views/Novels.vue
+++ src/views/Novels.vue
@@ -8,12 +8,22 @@ Notice that the v-html attribute of the article text does not start with a colon
         <EditableDiv identity="novels-1"></EditableDiv>
       </b-col>
     </b-row>
-    <b-row>
+    <b-row id="rolls">
       <b-col>
-        <TumblrBlock trimLength=0 redirect-location="" id="blog-roll" blogType="article"></TumblrBlock>
+        <TumblrBlock
+            trimLength=0
+            redirect-location="https://dailydilettante.tumblr.com/"
+            id="blog-roll"
+            blogType="article"
+            maxNumPosts=3></TumblrBlock>
       </b-col>
       <b-col>
-        <TumblrBlock trimLength=0 redirect-location="" id="vid-roll" blogType="video"></TumblrBlock>
+        <TumblrBlock
+            trimLength=0
+            redirect-location="https://dailydilettante.tumblr.com/"
+            id="vid-roll"
+            blogType="video"
+            maxNumPosts=3></TumblrBlock>
       </b-col>
     </b-row>
   </b-container>
@@ -28,12 +38,12 @@ export default {
   components: {TumblrBlock, EditableDiv},
   data() {
     return {
-      videos: [
-        {vidSrc: "https://www.youtube.com/embed/5O4Y9TbyEag"},
-        {vidSrc: "https://www.youtube.com/embed/99Hly9Cr8Q4"},
-        {vidSrc: "https://www.youtube.com/embed/9puKSBPY_cg"},
-        {vidSrc: "https://www.youtube.com/embed/GypebgAtyuY"}
-      ]
+      // videos: [
+      //   {vidSrc: "https://www.youtube.com/embed/5O4Y9TbyEag"},
+      //   {vidSrc: "https://www.youtube.com/embed/99Hly9Cr8Q4"},
+      //   {vidSrc: "https://www.youtube.com/embed/9puKSBPY_cg"},
+      //   {vidSrc: "https://www.youtube.com/embed/GypebgAtyuY"}
+      // ]
     }
   }
 }
@@ -42,6 +52,26 @@ export default {
 <style lang="scss">
 //noinspection CssUnknownTarget
 @import "@/assets/livery";
+
+#rolls {
+  .post {
+    border-radius: 5px;
+    box-shadow: 5px 5px 10px black;
+    margin: .25em;
+
+    .postHolder {
+      padding: .25em;
+    }
+  }
+
+  .more {
+    font-size: 1.5em;
+  }
+
+  .postListTail {
+    margin: 1em;
+  }
+}
 
 #blog-roll {
   img {
@@ -52,6 +82,7 @@ export default {
 
   .post {
     clear: both;
+    border-top: #05423C solid thin;
   }
 
   .article-text {
