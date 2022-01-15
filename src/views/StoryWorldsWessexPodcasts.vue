11cd42808a94860a921e247e4ae2f8d3295debd7
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2022-01-15 14:52:28 +0000

    added podcast feed reader to the wessex podcasts page

--- src/views/StoryWorldsWessexPodcasts.vue
+++ src/views/StoryWorldsWessexPodcasts.vue
@@ -1,29 +1,38 @@
 <template>
   <div id="storyWorlds">
-    <!--suppress HtmlUnknownBooleanAttribute -->
     <b-container id="wessex-podcasts">
       <h1>Storyworlds</h1>
       <h2>Thomas Hardy's Wessex Dramas</h2>
       <h3>A) Dramatic Podcast Releases in 2022</h3>
-      <b-card v-for="data in panels" class="series">
-        <img class="mr-3"
-             :src="require('@/assets/wessex/podcasts/' + data.poster)"
-             alt="Poster for the podcast"
-             width="128"
-        >
-        <h4 class="mt-0">{{ data.title }}<span class="dueDate">({{ data.dueDate }})</span></h4>
-
-        <p v-for="para in data.synopsis">{{ para }}</p>
-        <div class="row">
-          <audio v-for="episode in data.episodeList" controls class="w-75">
-            <source :src="require('@/assets/wessex/podcasts/' + episode.link + '.mp3')" type="audio/mpeg">
-            <source :src="require('@/assets/wessex/podcasts/' + episode.link + '.ogg')" type="audio/ogg">
-            <source :src="require('@/assets/wessex/podcasts/' + episode.link + '.wav')" type="audio/wav">
-            There's a surprise, your browser does not support the audio tag
-          </audio>
+      <b-card v-for="book in books" class="series">
+        <div class="bookDetails">
+          <img class="mr-3"
+               :src="require('@/assets/wessex/podcasts/' + book.poster)"
+               alt="Poster for the podcast"
+               width="128"
+          >
+          <h4 class="mt-0">{{ book.title }}<span class="dueDate">({{ book.dueDate }})</span></h4>
+          <p v-for="para in book.synopsis" class="book_synopsis">{{ para }}</p>
         </div>
+
+        <div class="bookPodcasts">
+          <div class="seasonPodcastList card-deck">
+            <div v-for="episode in book.episodeList" class="card podcastEpisode">
+              <div v-if="episode" class="m-auto">
+                <span v-if="episode.episodeNumber >= 0" class="episodeNumber">{{ episode.episodeNumber }}</span>
+                <audio controls preload="metadata" v-bind:src="episode.linkSrc">
+                  <source v-bind:src="episode.linkSrc" :type="episode.linkType">
+                  It appears that your browser can not play this,
+                  please try going to <a href="https://thedailydilettante.podbean.com/">https://thedailydilettante.podbean.com/</a>
+                  for the archive on Podbean.
+                </audio>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div class="row">
-          <a :href="data.screenplayLink"
+          <a :href="book.screenplayLink"
              target="_blank"
              class="list-item-additional-data ext-link"
              title="See the screenplay in a new tab"
@@ -32,15 +41,18 @@
 
       </b-card>
     </b-container>
+
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   name: "WessexPodcasts",
   data() {
     return {
-      panels: [
+      books: [
         {
           title: "Desperate Remedies",
           poster: "desperate-remedies/desperate-remedies-poster.jpg",
@@ -48,11 +60,7 @@ export default {
           synopsis: ["Cytherea, an orphaned teenaged girl, is forced to become a paid companion to a wealthy woman, Miss Aldclyffe. Cytherea’s fiancé is tricked away from her, and she is pressured into marrying Miss Aldclyffe’s own illegitimate, already-married son.",
             "Cytherea desperately tries to discover if she, herself, truly is married, or not, and how many “wives” are actually involved ….. all before this heir gets his hands on her"
           ],
-          episodeList: [
-            {
-              link: "desperate-remedies/DesperateRemediesMainTheme"
-            }
-          ],
+          episodeList: [],
           screenplayLink: "https://www.dropbox.com/s/8678tahc96nqulf/Desperate%20remedies%20-%20podcasted%20-%20screenplay.pdf?dl=0"
         },
         {
@@ -62,11 +70,7 @@ export default {
           synopsis: ["Ethelberta, is an ex-governess from a large servant family. She is already (at 21 years old) widowed, after having married Lady Petherwin's son. When her mother-in-law dies, she attempts to earn her living, dramatising her own poetry. In London, she moves her many siblings, and mother, into her house, passing them off as her own servants in an attempt to disguise her straightened means.",
             "Unfortunately, her young sister, Picotee, falls In love with one of Ethelbert's suitors. Complications ensue."
           ],
-          episodeList: [
-            {
-              link: "desperate-remedies/DesperateRemediesMainTheme"
-            }
-          ],
+          episodeList: [],
           screenplayLink: "https://www.dropbox.com/s/sx6e2dcc6y7bv3p/The%20Hand%20Of%20Ethelberta.pdf?dl=0"
         },
         {
@@ -76,15 +80,47 @@ export default {
           synopsis: ["George Somerset, a new Architecture graduate, asks to sketch the interior of a castle. He ends up falling in love with its millionaire owner, Paula Power: a very 'Modern Miss' with decided opinions of her own.",
             "Unfortunately there are malignant influences amongst the former aristocratic castle-owners, and the course of love does not run so smooth."
           ],
-          episodeList: [
-            {
-              link: "desperate-remedies/DesperateRemediesMainTheme"
-            }
-          ],
+          episodeList: [],
           screenplayLink: "https://www.dropbox.com/s/zbg3snqs8vyn49p/2021-12-28%20A%20Laodicean%20-%20podcasted.pdf?dl=0"
         }
       ]
     }
+  },
+  mounted() {
+    fetch('https://feed.podbean.com/thedailydilettante/feed.xml')
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+              const itemList = data.querySelectorAll('item')
+              itemList.forEach(item => {
+                console.log("processing an item")
+                try {
+                  const episodeIndex = item.getElementsByTagName('itunes\:episode')[0].textContent - 1
+                  const bookIndex = item.getElementsByTagName('itunes\:season')[0].textContent - 1
+
+                  const audInfo = item.getElementsByTagName('enclosure')[0]
+                  const audSrc = audInfo.getAttribute('url')
+                  const audSize = audInfo.getAttribute('length')
+                  const audType = audInfo.getAttribute('type')
+
+                  // const thumb = item.getElementsByTagName('itunes\:image')[0].getAttribute('href')
+                  const episodeDefinition = {
+                    episodeNumber: episodeIndex + 1,
+                    linkSrc: audSrc,
+                    linkSize: audSize,
+                    linkType: audType
+                  }
+
+                  // This is creating new elements that do not exist at the start, so set() is essential
+                  Vue.set(this.books[bookIndex].episodeList, episodeIndex, episodeDefinition)
+                } catch (err) {
+                  console.log("Failed to process one of the episodes. Going to try to continue with the remaining episodes")
+                  console.log(err.message)
+                }
+              }) // ends loop
+            }
+        )
+    //end mounted
   }
 }
 </script>
@@ -93,6 +129,27 @@ export default {
 @import "src/assets/livery";
 
 #storyWorlds {
+
+  .bookDetails {
+    overflow: auto; /* this forces the block containing the podcasts to cleat the image, even if there is not a lot of text*/
+  }
+
+  .seasonPodcastList {
+    margin: auto;
+
+    .podcastEpisode {
+      border: none;
+      box-shadow: none;
+      padding-left: 1em;
+      /* it is important to set a min width here, otherwise bootstrap can try to overlap these on smallish screens */
+      min-width: 20em;
+
+      .episodeNumber {
+        vertical-align: top;
+        margin-right: 0.5em;
+      }
+    }
+  }
 
   .card {
     background-color: transparent;
@@ -115,19 +172,19 @@ export default {
         font-weight: normal;
       }
 
-      .row {
-        audio {
-          margin: 0.5em auto auto;
-        }
-
-        a {
-          margin: auto
-        }
-
-        .list-item-additional-data {
-          padding-top: .75em;
-        }
+      audio {
+        margin: 0.5em auto auto;
+        height: 2em;
       }
+
+      a {
+        margin: auto
+      }
+
+      .list-item-additional-data {
+        padding-top: .75em;
+      }
+
     }
   }
 }
